4b837bdb7b16f53990aa7548d884e9e1083b9737
JohnAlbin <virtually.johnalbin@gmail.com>
2012-10-15 17:30:07 +0800

    Improve docs for config.rb

--- config.rb
+++ config.rb
@@ -1,25 +1,32 @@
+# First, require any additional compass plugins installed on your system.
+require 'zen-grids'
+# require 'breakpoint'
 
-# Change this to :production when ready to deploy the CSS to the live server.
-#environment = :development
+
+# Toggle this between :development and :production when deploying the CSS to the
+# live server. Development mode will retain comments and spacing from the
+# original Sass source and adds line numbering comments for easier debugging.
 environment = :production
+# environment = :development
 
 # In development, we can turn on the FireSass-compatible debug_info.
 firesass = false
-#firesass = true
+# firesass = true
 
 
-# Location of the theme's resources.
+# Location of the your project's resources.
 css_dir         = "css"
 sass_dir        = "sass"
 extensions_dir  = "sass-extensions"
 images_dir      = "images"
 javascripts_dir = "js"
 
+# Set this to the root of your project. All resource locations above are
+# considered to be relative to this path.
+http_path = "/"
 
-# Require any additional compass plugins installed on your system.
-#require 'ninesixty'
-require 'zen-grids'
-
+# To use relative paths to assets in your compiled CSS files, set this to true.
+# relative_assets = true
 
 
 ##
@@ -29,11 +36,6 @@ require 'zen-grids'
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
 output_style = (environment == :development) ? :expanded : :compressed
-
-# To enable relative paths to assets via compass helper functions. Since Drupal
-# themes can be installed in multiple locations, we don't need to worry about
-# the absolute path to the theme from the server root.
-relative_assets = true
 
 # To disable debugging comments that display the original location of your selectors. Uncomment:
 # line_comments = false
