d46f14503fbb17efa10c789c7349a8d578782463
JohnAlbin <virtually.johnalbin@gmail.com>
2013-02-19 02:16:43 +0800

    Re-add :path to Compass registration. Fixes unpack error.

--- lib/zen-grids.rb
+++ lib/zen-grids.rb
@@ -4,4 +4,9 @@ project_path     = File.join(File.dirname(__FILE__), '..')
 stylesheets_path = File.join(project_path)
 templates_path   = File.join(project_path, 'templates')
 
-Compass::Frameworks.register('zen-grids', :stylesheets_directory => stylesheets_path, :templates_directory => templates_path)
+Compass::Frameworks.register(
+  'zen-grids',
+  :path => project_path,
+  :stylesheets_directory => stylesheets_path,
+  :templates_directory => templates_path
+)
