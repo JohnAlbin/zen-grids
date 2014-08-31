2e067a46c0277e537000b29b74d54be6e2b8fc47
JohnAlbin <virtually.johnalbin@gmail.com>
2014-08-31 11:07:14 +0800

    Move partials into stylesheets folder to prevent Compass from rendering tests and templates.

--- lib/zen-grids.rb
+++ lib/zen-grids.rb
@@ -1,7 +1,7 @@
 require 'compass'
 
 project_path     = File.join(File.dirname(__FILE__), '..')
-stylesheets_path = File.join(project_path)
+stylesheets_path = File.join(project_path, 'stylesheets')
 templates_path   = File.join(project_path, 'templates')
 
 Compass::Frameworks.register(
