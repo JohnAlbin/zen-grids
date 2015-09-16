c3b7469d2570c30c02a3257767e16f67ff83fa88
JohnAlbin <virtually.johnalbin@gmail.com>
2015-09-17 06:40:19 +0800

    Remove remanents of templates folder.

--- lib/zen-grids.rb
+++ lib/zen-grids.rb
@@ -2,11 +2,9 @@ require 'compass'
 
 project_path     = File.join(File.dirname(__FILE__), '..')
 stylesheets_path = File.join(project_path, 'sass')
-templates_path   = File.join(project_path, 'templates')
 
 Compass::Frameworks.register(
   'zen-grids',
   :path => project_path,
-  :stylesheets_directory => stylesheets_path,
-  :templates_directory => templates_path
+  :stylesheets_directory => stylesheets_path
 )
