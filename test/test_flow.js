e6086b088c6a48f79f9219d320cbb4e213412720
JohnAlbin <virtually.johnalbin@gmail.com>
2015-10-26 22:17:06 +0800

    Change zen-flow-item-width() warning into error.

--- test/test_flow.js
+++ test/test_flow.js
@@ -10,10 +10,17 @@ describe('@import "zen-grids/flow";', function() {
   });
 
   describe('@mixin zen-flow-item-width()', function() {
+    it('should error on responsive layouts with no $parent-span', function(done) {
+      sassyTest.renderFixture('zen-flow-item-width/error', {}, function(error, result, expectedOutput) {
+        error.should.exist;
+        error.message.should.equal('For responsive layouts with a percentage-based grid width, you must set the $parent-column-span to the number of columns that the parent element spans.');
+        done();
+      });
+    });
+
     it('should pass legacy tests', function(done) {
-      sassyTest.renderFixture('zen-flow-item-width', {}, function(error, result, expectedOutput) {
+      sassyTest.renderFixture('zen-flow-item-width/legacy', {}, function(error, result, expectedOutput) {
         should.not.exist(error);
-        result.warn[0].should.equal('For responsive layouts with a percentage-based grid width, you must set the $parent-column-span to the number of columns that the parent element spans.');
         done();
       });
     });
