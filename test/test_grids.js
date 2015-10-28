0cd65d63a44d3b17335fa11f21ab1af8c656eca4
JohnAlbin <virtually.johnalbin@gmail.com>
2015-10-28 22:56:25 +0800

    Convert some legacy tests.

--- test/test_grids.js
+++ test/test_grids.js
@@ -28,17 +28,67 @@ describe('@import "zen-grids/grids";', function() {
   });
 
   describe('@mixin zen-grid-item-base()', function() {
-    it('should pass legacy tests', function(done) {
-      sassyTest.renderFixture('zen-grid-item-base', {}, function (error, result, expectedOutput) {
+    it('should add grid item base', function(done) {
+      sassyTest.renderFixture('zen-grid-item-base/gutters', {}, function (error, result, expectedOutput) {
         should.not.exist(error);
+        done();
+      });
+    });
+
+    it('should use gutter-method option', function(done) {
+      sassyTest.renderFixture('zen-grid-item-base/gutter-method', {}, function (error, result, expectedOutput) {
+        should.not.exist(error);
+        done();
+      });
+    });
+
+    it('should use box-sizing option', function(done) {
+      sassyTest.renderFixture('zen-grid-item-base/box-sizing', {}, function (error, result, expectedOutput) {
+        should.not.exist(error);
+        done();
+      });
+    });
+
+    it('should use direction option', function(done) {
+      sassyTest.renderFixture('zen-grid-item-base/direction', {}, function (error, result, expectedOutput) {
+        should.not.exist(error);
+        done();
+      });
+    });
+
+    it('should use switch-direction option', function(done) {
+      sassyTest.renderFixture('zen-grid-item-base/switch-direction', {}, function (error, result, expectedOutput) {
+        should.not.exist(error);
+        done();
+      });
+    });
+
+    it('should support IE 7', function(done) {
+      sassyTest.renderFixture('zen-grid-item-base/ie', {}, function (error, result, expectedOutput) {
+        should.not.exist(error);
+        done();
+      });
+    });
+
+    it('should warn about IE 7 support', function(done) {
+      sassyTest.renderFixture('zen-grid-item-base/warn-box-sizing-polyfill-path', {}, function (error, result, expectedOutput) {
+        should.not.exist(error);
+        result.warn[0].should.equal('Setting $box-sizing to border-box will fail for IE 7 and earlier because the $box-sizing-polyfill-path is empty.');
         done();
       });
     });
   });
 
   describe('@mixin zen-new-row()', function() {
-    it('should pass legacy tests', function(done) {
-      sassyTest.renderFixture('zen-new-row', {}, function (error, result, expectedOutput) {
+    it('should add clear', function(done) {
+      sassyTest.renderFixture('zen-new-row/clear', {}, function (error, result, expectedOutput) {
+        should.not.exist(error);
+        done();
+      });
+    });
+
+    it('should switch direction of clear', function(done) {
+      sassyTest.renderFixture('zen-new-row/switch-direction', {}, function (error, result, expectedOutput) {
         should.not.exist(error);
         done();
       });
@@ -46,8 +96,22 @@ describe('@import "zen-grids/grids";', function() {
   });
 
   describe('@mixin zen-apply-gutter-padding()', function() {
-    it.skip('should have some tests', function(done) {
-      sassyTest.renderFixture('zen-apply-gutter-padding', {}, function (error, result, expectedOutput) {
+    it('should add gutter padding', function(done) {
+      sassyTest.renderFixture('zen-apply-gutter-padding/gutters', {}, function (error, result, expectedOutput) {
+        should.not.exist(error);
+        done();
+      });
+    });
+
+    it('should add directional padding', function(done) {
+      sassyTest.renderFixture('zen-apply-gutter-padding/direction', {}, function (error, result, expectedOutput) {
+        should.not.exist(error);
+        done();
+      });
+    });
+
+    it('should switch directional padding', function(done) {
+      sassyTest.renderFixture('zen-apply-gutter-padding/switch-direction', {}, function (error, result, expectedOutput) {
         should.not.exist(error);
         done();
       });
