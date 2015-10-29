4c89ee48149bd12e28e01fbdb5284e0c6f4f7dd2
JohnAlbin <virtually.johnalbin@gmail.com>
2015-10-29 10:45:33 +0800

    Add tests to check for node-sass-import-once and Eyeglass compatibility.

--- test/test_3rd_party_integration.js
+++ test/test_3rd_party_integration.js
@@ -1,5 +1,9 @@
 'use strict';
 
+var Eyeglass = require('eyeglass').Eyeglass;
+var eyeglass = new Eyeglass({});
+var importOnce = require('node-sass-import-once');
+
 describe('compatibility with other libraries', function() {
   before(function(done) {
     sassyTest.configurePaths({
@@ -12,6 +16,31 @@ describe('compatibility with other libraries', function() {
   describe('Compass', function() {
     it('should work with Compass', function(done) {
       sassyTest.renderFixture('compass', {}, function(error, result, expectedOutput) {
+        should.not.exist(error);
+        done();
+      });
+    });
+  });
+
+  describe('Eyeglass', function() {
+    it('should work with Eyeglass', function(done) {
+      sassyTest.renderFixture('eyeglass', eyeglass.sassOptions(), function(error, result, expectedOutput) {
+        should.not.exist(error);
+        done();
+      });
+    });
+  });
+
+  describe('node-sass-import-once', function() {
+    it('should work with node-sass-import-once', function(done) {
+      sassyTest.renderFixture('node-sass-import-once', {
+        importer: importOnce,
+        importOnce: {
+          index: false,
+          css: false,
+          bower: false
+        }
+      }, function(error, result, expectedOutput) {
         should.not.exist(error);
         done();
       });
